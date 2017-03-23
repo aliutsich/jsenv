9d9f718663f786d501aa2286fcf1ab949ae4c371
aliutsich <aliutsich@gmail.com>
2017-03-24 02:09:23 +0300

    4th

--- src/index.test.js
+++ src/index.test.js
@@ -9,11 +9,11 @@ describe('Our first test', () => {
 });
 
 describe('index.html', () => {
-  it('should say hello', () => {
+  it('should say "Users"', () => {
     const index = fs.readFileSync('./src/index.html', "utf-8");
     jsdom.env(index, (err, window) => {
       const h1 = window.document.getElementsByTagName('h1')[0];
-      expect(h1.innerHTML).to.equal('Fuck the World');
+      expect(h1.innerHTML).to.equal('Users');
       window.close();
     });
   })
